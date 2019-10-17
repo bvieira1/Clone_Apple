7b123d2ef02286f658b28531837ea82ca7c9687e
bvieira1 <cristianovieirati@gmail.com>
2019-10-16 23:36:27 -0300

    Inicio do projeto clone do site da Apple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Iphone11Component } from './iphone11/iphone11.component';
+import { WatchComponent } from './watch/watch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    Iphone11Component,
+    WatchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/iphone11/iphone11.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iphone11',
+  templateUrl: './iphone11.component.html',
+  styleUrls: ['./iphone11.component.scss']
+})
+export class Iphone11Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
